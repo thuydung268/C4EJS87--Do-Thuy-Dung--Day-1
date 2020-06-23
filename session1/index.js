57fcb00025fbeae7671422c661ad30d6bed33964
thuydung123 <thuydungdo268@gmail.com>
2020-06-23 20:51:12 +0700

    first commit

---
+++ session1/index.js
@@ -0,0 +1,123 @@
+// alert('hello world 87');
+// console.log('hello world');
+
+// let x = 5;
+// s = 'hello world';
+// console.log(x);
+// console.log(s);
+
+// x = 6;
+// s = 'hi there';
+// x = prompt('nhap x');
+// s = prompt('nhap s');
+// console.log(x);
+// console.log(s);
+
+// x = 5+6;
+// console.log(x);
+
+// let x = 5
+// x++;
+// console.log(x);
+
+// let x = Math.floor(5.6);
+// console.log(x);
+
+// let x = "chao ban"
+// x = x.toUpperCase();
+// console.log(x);
+// console.log(`I have ${5*5} dogs`)
+
+// let n = Number('45');
+// n = n + 5; 
+// console.log(n);
+
+// let s = String(198);
+// s = s + 5;
+// console.log(s);
+
+// let x = 0;
+// while (x < 3) 
+// {
+//         console.log("I Love You");
+//         x++;
+// }
+
+// for(let x = 0; x < 3; x++) {
+//         console.log("I love you");
+// }
+
+// for(let i=0;i<7;i++){
+//     console.log(i);
+// };
+
+// let age = 9;
+// if (age<10) {
+//     console.log('nho hon 10');
+// }else {
+//     console.log ('lon hon 10');
+// }
+
+// let age = 9;
+// if (age < 10) {
+//     console.log ('nho hon 10');
+// } else if (age < 18){
+// console.log ('nho hon 18');
+// } else {
+//     console.log ('lon hon 18');
+// }
+
+// let message = 'coding is great';
+// message = 'Coding might not be easy, but still great';
+// console.log (message);
+
+// let studentCount = 0;
+// console.log (studentCount);
+
+// Let message = ‘Coding might not be easy but still great’;
+// message = message.tolowercase;
+// console.log (message);
+// console.log ('I have $ 5*5 dogs')
+
+// studentCount = 16;
+// studentCount ++;
+// console.log (studentCount);
+
+// alert('hello world 87');
+// console.log('hello world');
+
+// alert('you look beautiful today');
+
+// x = 6;
+// s = 'hi there';
+// x = prompt('nhap x');
+// s = prompt('nhap s');
+// console.log(x);
+// console.log(s);
+
+// let s = 'hi there, your name please?';
+// s = prompt(s);
+// alert('hi' + " " + s);
+
+// let s = 'Enter your first name';
+// x = 'Enter your last name';
+// s = prompt(s);
+// x = prompt(x);
+// alert('hi' + " " + x + " " + s);
+
+// let x = 'Enter side length of a square';
+// x = prompt(x);
+// alert('the square area is' + " " + "x*x");
+
+// let x = 'Enter the radius of the circle';
+// x = prompt(x);
+// alert('the circle area is' + " " + 3.14 * x*x);
+
+// let x = 'Enter the temperature in Celcius';
+// x = prompt (x);
+// alert (x = 9/5 * x - 32);
+
+let x =prompt('type temperature in Celcius');
+let y =(x*9/5)+32;
+alert("gia tri do F la " + y );
+~
